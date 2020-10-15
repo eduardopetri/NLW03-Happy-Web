1dde9397c7d8f2d729414dcd938c9c5a58a96323
eduardopetri <eduardoafonsopetri@gmail.com>
2020-10-14 22:12:20 -0300

    Second day next level week

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes() {
   return (
@@ -9,6 +11,9 @@ function Routes() {
       <Switch>
         <Route path="/" component={Landing} exact />
         <Route path="/app" component={OrphanagesMap} exact />
+
+        <Route path="/orphanages/create" component={CreateOrphanage} exact />
+        <Route path="/orphanages/:id" component={Orphanage} exact />
       </Switch>
     </BrowserRouter>
   );
